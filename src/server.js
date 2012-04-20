63c401b085f807eca2087db7d43c63dcb8838184
Matthew Lane <lanemt@gmail.com>
2012-04-19 21:50:52 -0400

    now with server module and index.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,19 @@
 
-var http = require('http');
+var http = require('http')
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('Hello World');
-  response.end();
-}).listen(8888);
+function start() {
 
+  var port = 8888
+  
+  function onRequest(request, response) {
+    console.log('Request received.')
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.write('Hello World')
+    response.end()
+  }
+
+  http.createServer(onRequest).listen(port)
+  console.log('Server has started on port ' + port + '.')
+}
+
+exports.start = start

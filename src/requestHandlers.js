c1b85408d1d5d5a96f9be9ef512cf28946a26ebf
Matthew Lane <lanemt@gmail.com>
2012-04-19 23:04:01 -0400

    adds non-blocking operation support to request handlers
    injection of response object into handlers
    
    adds 404 response when handler cannot be found

--- src/requestHandlers.js
+++ src/requestHandlers.js
@@ -1,10 +1,21 @@
 
-var start = function() {
+var exec = require('child_process').exec
+
+var start = function(response) {
   console.log('Request handler \'start\' was called.')
+
+  exec('ls -lah', function (error, stdout, stderr) {
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.write(stdout)
+    response.end()
+  })
 }
 
-var upload = function() {
+var upload = function(response) {
   console.log('Request handler \'upload\' was called.')
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.write('Hello Upload')
+  response.end()
 }
 
 exports.start = start

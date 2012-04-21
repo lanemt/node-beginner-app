5aeae968701b6b7321f6c9f97ee53a9368ffef62
Matthew Lane <lanemt@gmail.com>
2012-04-21 08:28:31 -0400

    adds simple form and POST handling

--- src/requestHandlers.js
+++ src/requestHandlers.js
@@ -1,20 +1,31 @@
 
-var exec = require('child_process').exec
-
-var start = function(response) {
+var start = function(response, postData) {
   console.log('Request handler \'start\' was called.')
 
-  exec('ls -lah', function (error, stdout, stderr) {
-    response.writeHead(200, {'Content-Type': 'text/plain'})
-    response.write(stdout)
-    response.end()
-  })
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; '+
+    'charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>'
+
+
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.write(body)
+  response.end()
 }
 
-var upload = function(response) {
+var upload = function(response, postData) {
   console.log('Request handler \'upload\' was called.')
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello Upload')
+  response.write('Hello Upload\n')
+  response.write('You sent: ' + postData)
   response.end()
 }
 
--- src/server.js
+++ src/server.js
@@ -7,10 +7,21 @@ function start(route, handle) {
   var port = 8888
   
   var onRequest = function (request, response) {
-    var pathname = url.parse(request.url).pathname;
+    var pathname = url.parse(request.url).pathname,
+        postData = ''
     console.log('Request for \'' + pathname + '\' received.')
 
-    route(handle, pathname, response)
+    request.setEncoding('utf8')
+
+    request.addListener('data', function(postDataChunk) {
+      postData += postDataChunk
+      console.log('Received POST data chunk \'' + postDataChunk + '\'.')
+    })
+
+    request.addListener('end', function() {
+      route(handle, pathname, response, postData)
+    })
+
   }
 
   http.createServer(onRequest).listen(port)
